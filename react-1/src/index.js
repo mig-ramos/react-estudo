ebf50082d04340f90ed23a47b6be5ea0adae9866
Miguel Arcanjo <mig.ramos10@gmail.com>
2022-02-07 20:15:17 -0300

    Inicio de um App React

--- react-1/src/index.js
+++ react-1/src/index.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello, React!</h1>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
