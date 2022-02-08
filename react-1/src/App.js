e227b905075e97ebc0168cc395ce043bd3474b91
Miguel Arcanjo <mig.ramos10@gmail.com>
2022-02-07 21:19:54 -0300

    Criado um componente Table

--- react-1/src/App.js
+++ react-1/src/App.js
@@ -1,13 +1,14 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import Table from './Table'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello, React!</h1>
-      </div>
-    )
+    render() {
+      return (
+        <div className="container">
+          <Table />
+        </div>
+      )
+    }
   }
-}
 
 export default App
